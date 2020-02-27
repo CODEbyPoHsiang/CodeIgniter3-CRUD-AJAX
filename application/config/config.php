6edbf2fa1fb4cfe19a01b8ba34dbbc71746b1c6b
CODEbyPoHsiang <52902326+CODEbyPoHsiang@users.noreply.github.com>
2020-02-27 13:37:22 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci3crud/';
+$config['base_url'] = 'http://localhost/CodeIgniter3-CRUD-AJAX/';
 
 /*
 |--------------------------------------------------------------------------
